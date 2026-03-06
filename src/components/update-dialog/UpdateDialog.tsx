06656827cece8d5fc598130defefa7998b689332
AlexShatokhin <sashatokhin.04@gmail.com>
2026-03-06 11:45:45 +0200

    fix(dialog): fix modal

--- src/components/update-dialog/UpdateDialog.tsx
+++ src/components/update-dialog/UpdateDialog.tsx
@@ -14,15 +14,14 @@ export const UpdateDialog = () => {
 
     useEffect(() => {
         const lastVersion = localStorage.getItem("last-version");
-        console.log(lastVersion)
         if(lastVersion !== "1.3.0"){
-            setIsShow(true);
-            localStorage.setItem("last-version", "1.3.0");
+            setTimeout(() => setIsShow(true), 1000) 
         }
     }, [])
 
     const handleClose = () => {
         setIsShow(false);
+        localStorage.setItem("last-version", "1.3.0");
     }
 
     return (
@@ -30,13 +29,13 @@ export const UpdateDialog = () => {
       <AlertDialogContent>
         <AlertDialogHeader>
           <AlertDialogTitle>Новая версия калькулятора</AlertDialogTitle>
-          <AlertDialogDescription className="text-left">
+          <AlertDialogDescription className="text-left" asChild>
             <div>
-                <span className="mb-2">В новой версии калькулятора:</span> <br />
+                <span className="block">В новой версии калькулятора:</span> <br />
                 <div>
                     <p>Изменена операция деления с остатком. Отрицательные числа теперь обрабатываются корректно</p>
                     <br />
-                    <p>В режиме <span className="bg-blue-950">x <sup>y</sup> % z</span> в степени единица вводится по умолчанию</p>
+                    <p>В режиме <span className="bg-blue-950 px-2 rounded">x <sup>y</sup> % z</span> в степени <strong>1</strong> вводится по умолчанию</p>
                 </div>
             </div>
           </AlertDialogDescription>
